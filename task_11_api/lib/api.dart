917125a16d6b078cca69e14cc59f1420378cdc19
koooge <koooooge@gmail.com>
2020-01-15 20:23:16 +0100

    23. Quiz: Add the Flutter Currency COnverter API
    Signed-off-by: koooge <koooooge@gmail.com>

--- task_11_api/lib/api.dart
+++ task_11_api/lib/api.dart
@@ -2,7 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO: Import relevant packages
+import 'dart:async';
+import 'dart:convert' show json, utf8;
+import 'dart:io';
+
+const apiCategory = {
+  'name': 'Currency',
+  'route': 'currency',
+};
 
 /// The REST API retrieves unit conversions for [Categories] that change.
 ///
@@ -13,20 +20,56 @@
 ///   GET /currency: get a list of currencies
 ///   GET /currency/convert: get conversion from one currency amount to another
 class Api {
+  final HttpClient _httpClient = HttpClient();
 
-  // TODO: Add any relevant variables and helper functions
+  final String _url = 'flutter.udacity.com';
 
-  // TODO: Create getUnits()
   /// Gets all the units and conversion rates for a given category.
   ///
   /// The `category` parameter is the name of the [Category] from which to
   /// retrieve units. We pass this into the query parameter in the API call.
   ///
   /// Returns a list. Returns null on error.
+  Future<List> getUnits(String category) async {
+    final uri = Uri.https(_url, '/$category');
+    final jsonResponse = await _getJson(uri);
+    if (jsonResponse == null || jsonResponse['units'] == null) {
+      print('Error retrieving units.');
+      return null;
+    }
+    return jsonResponse['units'];
+  }
 
-  // TODO: Create convert()
   /// Given two units, converts from one to another.
   ///
   /// Returns a double, which is the converted amount. Returns null on error.
+  Future<double> convert(
+      String category, String amount, String fromUnit, String toUnit) async {
+    final uri = Uri.https(_url, '/$category/convert',
+      {'amount': amount, 'from': fromUnit, 'to': toUnit});
+    final jsonResponse = await _getJson(uri);
+    if (jsonResponse == null || jsonResponse['status'] == null) {
+      print('Error retrieving conversion.');
+      return null;
+    } else if (jsonResponse['status'] == 'error') {
+      print(jsonResponse['message']);
+      return null;
+    }
+    return jsonResponse['conversion'].toDouble();
+  }
 
+  Future<Map<String, dynamic>> _getJson(Uri uri) async {
+    try {
+      final httpRequest = await _httpClient.getUrl(uri);
+      final httpResponse = await httpRequest.close();
+      if (httpResponse.statusCode != HttpStatus.ok) {
+        return null;
+      }
+      final responseBody = await httpResponse.transform(utf8.decoder).join();
+      return json.decode(responseBody);
+    } on Exception catch (e) {
+      print('$e');
+      return null;
+    }
+  }
 }
--- task_11_api/lib/category_route.dart
+++ task_11_api/lib/category_route.dart
@@ -7,7 +7,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-// TODO: Import necessary package
+import 'api.dart';
 import 'backdrop.dart';
 import 'category.dart';
 import 'category_tile.dart';
@@ -92,7 +92,7 @@ class _CategoryRouteState extends State<CategoryRoute> {
     // We only want to load our data in once
     if (_categories.isEmpty) {
       await _retrieveLocalCategories();
-      // TODO: Call _retrieveApiCategory() here
+      await _retrieveApiCategory();
     }
   }
 
@@ -128,9 +128,34 @@ class _CategoryRouteState extends State<CategoryRoute> {
     });
   }
 
-  // TODO: Add the Currency Category retrieved from the API, to our _categories
   /// Retrieves a [Category] and its [Unit]s from an API on the web
-  Future<void> _retrieveApiCategory() async {}
+  Future<void> _retrieveApiCategory() async {
+    setState(() {
+      _categories.add(Category(
+        name: apiCategory['name'],
+        units: [],
+        color: _baseColors.last,
+        iconLocation: _icons.last,
+      ));
+    });
+    final api = Api();
+    final jsonUnits = await api.getUnits(apiCategory['route']);
+    if (jsonUnits != null) {
+      final units = <Unit>[];
+      for (var unit in jsonUnits) {
+        units.add(Unit.fromJson(unit));
+      }
+      setState(() {
+        _categories.removeLast();
+        _categories.add(Category(
+          name: apiCategory['name'],
+          units: units,
+          color: _baseColors.last,
+          iconLocation: _icons.last,
+        ));
+      });
+    }
+  }
 
   /// Function to call when a [Category] is tapped.
   void _onCategoryTap(Category category) {
